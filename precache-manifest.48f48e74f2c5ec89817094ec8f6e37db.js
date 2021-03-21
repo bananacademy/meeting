7eb0ec3128284defc3fb129e3a4c4ff9b970e1d7
bananacademy <bananacademyagora@gmail.com>
2021-03-21 15:45:29 +0200

    Updates

---
+++ precache-manifest.48f48e74f2c5ec89817094ec8f6e37db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c33029a86b6ad828cdff8baa145b0",
+    "url": "/meeting/7413aca8ce3f0854c29a.worker.js"
+  },
+  {
+    "revision": "481ca833cf7191760b3145d3e5b0b970",
+    "url": "/meeting/index.html"
+  },
+  {
+    "revision": "4f43b33924f30adba71e",
+    "url": "/meeting/static/css/2.cb0b8a9c.chunk.css"
+  },
+  {
+    "revision": "dbeaf28b783beef48802",
+    "url": "/meeting/static/css/main.34ce2a7f.chunk.css"
+  },
+  {
+    "revision": "4f43b33924f30adba71e",
+    "url": "/meeting/static/js/2.e5895442.chunk.js"
+  },
+  {
+    "revision": "14d1b9c68415ebdc50ce47b5fa22b464",
+    "url": "/meeting/static/js/2.e5895442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbeaf28b783beef48802",
+    "url": "/meeting/static/js/main.b198e396.chunk.js"
+  },
+  {
+    "revision": "a93a2642a083b9eb5516",
+    "url": "/meeting/static/js/runtime-main.2da4c845.js"
+  },
+  {
+    "revision": "2a72994b8a5a34fcb92ba89ffd862c4b",
+    "url": "/meeting/static/media/cover-cn.2a72994b.png"
+  },
+  {
+    "revision": "a2d244ac024c8c7f67a3a849597a5ba8",
+    "url": "/meeting/static/media/cover-us.a2d244ac.png"
+  },
+  {
+    "revision": "45b9371b21ff0d4e1ce607bc647ba267",
+    "url": "/meeting/static/media/pic-message@2x.45b9371b.png"
+  },
+  {
+    "revision": "f5d3055bb39ea0d98e47435a207b7040",
+    "url": "/meeting/static/media/pic-student.f5d3055b.png"
+  },
+  {
+    "revision": "580ff5f1a224a4b75cf57361e7f0d661",
+    "url": "/meeting/static/media/pic-teacher.580ff5f1.png"
+  }
+]);
